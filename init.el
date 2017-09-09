0f674d383ea85e8779b863bc87778ee0ff46f9bb
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2017-09-09 19:50:56 +0200

    First verison of .emacs.d init'ed from org file
    This contains the first version of a working init.el based on an
    myinit.org file, which autogenerates an myinit.el. This is done by
    org-babel-load-file automatically, if myinit.org changed, since last
    creation of myinit.el.

---
+++ init.el
@@ -0,0 +1,19 @@
+;; enable MELPA package repository
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+;; Botstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; load the custom settings created by emacs automatically
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file :noerror)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
